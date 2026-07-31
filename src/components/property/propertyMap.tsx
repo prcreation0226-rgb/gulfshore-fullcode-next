0c8481e6ef0bfbb5dfea5e8a87d6ab30c9b90712
Kiaan <lightlabcreation@gmail.com>
2026-07-31 15:34:24 +0530

    update sodle property

--- src/components/property/propertyMap.tsx
+++ src/components/property/propertyMap.tsx
@@ -73,8 +73,7 @@ export default function PropertyMap({ property, Latitude, Longitude }: PropertyM
 					if (!top || !bot) return null;
 
 					const bbox = `${top.lng()},${bot.lat()},${bot.lng()},${top.lat()}`;
-					const layerDefs = encodeURIComponent('{"28":"ZONE_SUBTY <> \'AREA OF MINIMAL FLOOD HAZARD\'"}');
-					return `https://hazards.fema.gov/gis/nfhl/rest/services/public/NFHL/MapServer/export?bbox=${bbox}&bboxSR=4326&layers=show%3A28&layerDefs=${layerDefs}&size=256,256&imageSR=3857&format=png32&transparent=true&f=image`;
+					return `https://hazards.fema.gov/gis/nfhl/rest/services/public/NFHL/MapServer/export?bbox=${bbox}&bboxSR=4326&size=256,256&imageSR=3857&format=png32&transparent=true&f=image`;
 				},
 				tileSize: new google.maps.Size(256, 256),
 				opacity: 0.35, // Made highly transparent so Satellite is visible
