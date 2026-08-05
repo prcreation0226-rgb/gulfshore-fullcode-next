3a50f3fd777f624686b574e602a204e8d702d7a3
Kiaan <lightlabcreation@gmail.com>
2026-08-05 16:50:54 +0530

    Hide carousel arrows on desktop until hovered

--- src/components/cards/property/cardCarousel.tsx
+++ src/components/cards/property/cardCarousel.tsx
@@ -94,12 +94,12 @@ export default function CardCarousel({
 							<CarouselPrevious
 								variant="outline"
 								size="icon"
-								className="left-1.5 z-20 cursor-pointer !bg-white/90 hover:!bg-white text-black border-0 shadow-md top-1/2 h-9 w-9 flex !opacity-90 md:!opacity-0 md:group-hover:!opacity-100 transition-opacity duration-300"
+								className="left-1.5 z-20 cursor-pointer !bg-white/90 hover:!bg-white text-black border-0 shadow-md top-1/2 h-9 w-9 flex opacity-90 md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-300"
 							/>
 							<CarouselNext
 								variant="outline"
 								size="icon"
-								className="right-1.5 z-20 cursor-pointer !bg-white/90 hover:!bg-white text-black border-0 shadow-md top-1/2 h-9 w-9 flex !opacity-90 md:!opacity-0 md:group-hover:!opacity-100 transition-opacity duration-300"
+								className="right-1.5 z-20 cursor-pointer !bg-white/90 hover:!bg-white text-black border-0 shadow-md top-1/2 h-9 w-9 flex opacity-90 md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-300"
 							/>
 						</div>
 					</Carousel>
